a11aac59bd6af5ec244c3f9e3fc56e3329c5bf8d
Oliver Stueker <ostueker@mun.ca>
2018-10-30 10:32:57 -0230

    Fixing the  bug, this leaves only the Python 2 bug for fixing

--- mean.py
+++ mean.py
@@ -1,6 +1,10 @@
 def mean(num_list):
     try:
-        return sum(num_list)/len(num_list)
+        result = sum(num_list)/len(num_list)
+        if type(result) == complex:
+            return NotImplemented
+        else:
+            return result
     except ZeroDivisionError as detail :
         msg = "The algebraic mean of an empty list is undefined."
         msg += "Please provide a list of numbers."
